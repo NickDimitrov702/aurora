458a3c2a8e11edf852c2ccca8d77e6f69735d914
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2022-03-22 21:34:17 +0200

    Creating Contacts component

---
+++ src/components/Content/links/contacts/Contacts.js
@@ -0,0 +1,12 @@
+import style from './Contacts.module.css'
+
+function Contacts() {
+
+    return (
+        <div className={style.contactWrapper}>
+            <p>Contacts</p>
+        </div>
+    )
+}
+
+export default Contacts
