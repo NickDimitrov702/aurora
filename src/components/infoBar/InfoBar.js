bd2c6c8c84553ba24cc15bcf03ecfa97a654adf1
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2022-04-03 15:53:40 +0300

    Creating collaps dropdown menue for mobile

--- src/components/infoBar/InfoBar.js
+++ src/components/infoBar/InfoBar.js
@@ -1,4 +1,7 @@
 import style from './InfoBar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faEnvelope, faMailBulk, faPhone } from '@fortawesome/free-solid-svg-icons'
+
 
 function InfoBar() {
 
@@ -6,9 +9,9 @@ function InfoBar() {
         <div className={style.infoBarWrapper}>
             <div className={style.infoBar}>
                 <ul className={style.infoBarInfo}>
-                    <li>+359 08923124</li>
-                    <li>botique@botique.com</li>
-                    <li>Working time: Fr-Mon 10:00 - 18:00</li>
+                    <li><FontAwesomeIcon className={style.infoBarIcon} icon={faPhone}></FontAwesomeIcon>+359 08923124</li>
+                    <li><FontAwesomeIcon className={style.infoBarIcon} icon={faEnvelope}></FontAwesomeIcon>botique@botique.com</li>
+                    <li><FontAwesomeIcon className={style.infoBarIcon} icon={faClock}></FontAwesomeIcon>Working time: Fr-Mon 10:00 - 18:00</li>
                 </ul>
             </div>
         </div>
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -9,12 +9,64 @@ import ScrollIntoView from 'react-scroll-into-view'
 
 function NavBar() {
 
+    let isDropped = false
+
+    function navBarCollaps() {
+
+        const collaps = document.getElementById('NavbarListItemsCollaps')
+
+        if (isDropped === false) {
+            collaps.style.display = 'flex'
+            isDropped = true
+        } else {
+            collaps.style.display = 'none';
+            isDropped = false
+        }
+
+        // if(isDropped === true) {
+
+        //     isDropped = false
+        // }
+        console.log('Fucka you')
+    }
 
     return (
         <nav className={style.NavBarWrapper}>
             <div className={style.line}></div>
             <div className={style.LogoWrapper}>
                 <img className={style.image} src={Logo}></img>
+            </div>
+            <div className={style.NavbarCollaps}>
+                <button className={style.collapsButn} onClick={navBarCollaps}>
+                    Menue
+                </button>
+                <ul className={style.NavbarListItemsCollaps} id='NavbarListItemsCollaps'>
+                    <li>
+                        <ScrollIntoView selector='#about'>
+                            <Link className={style.Collapslink} to='/about'>Gifts</Link>
+                        </ScrollIntoView>
+                    </li>
+                    <li>
+                        <ScrollIntoView selector='#about'>
+                            <Link className={style.Collapslink} to='/about'>Flowers</Link>
+                        </ScrollIntoView>
+                    </li>
+                    <li>
+                        <ScrollIntoView selector='#about'>
+                            <Link className={style.Collapslink} to='/about'>About</Link>
+                        </ScrollIntoView>
+                    </li>
+                    <li>
+                        <ScrollIntoView selector='#about'>
+                            <Link className={style.Collapslink} to='/about'>Products</Link>
+                        </ScrollIntoView>
+                    </li>
+                    <li>
+                        <ScrollIntoView selector='#about'>
+                            <Link className={style.Collapslink} to='/about'>Contacts</Link>
+                        </ScrollIntoView>
+                    </li>
+                </ul>
             </div>
             <ul className={style.listItemWrapper}>
                 <li><Link className={style.linkItems} to='/gifts'>Gifts</Link></li>
